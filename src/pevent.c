4026d12737906700ab801f6fc6bb6482ae442edd
Toni Uhlig <matzeton@googlemail.com>
2018-07-18 09:22:37 +0200

    Copyright info updated
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/pevent.c
+++ src/pevent.c
@@ -14,8 +14,8 @@
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
  *
- * - Neither the name of the Yellow Lemon Software nor the names of its
- *   contributors may be used to endorse or promote products derived from this
+ * - The names of its contributors may not be used to endorse or promote
+ *   products derived from this
  *   software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
